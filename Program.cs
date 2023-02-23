1fdc88c3a33158df19e761f2aa64dcfc4408b1c2
Abhishek-tara <abhishektara.ak47@gmail.com>
2023-02-23 13:56:48 +0530

    Added method to create Linked list

--- Program.cs
+++ Program.cs
@@ -5,6 +5,14 @@
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome to the Linked List Problems");
+
+            LinkedList linked = new LinkedList();
+            linked.Add(10);
+            linked.Add(20);
+            linked.Add(30);
+            linked.Add(50);
+
+            linked.Display();
         }
     }
 }
