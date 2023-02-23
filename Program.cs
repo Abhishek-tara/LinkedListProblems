2191b9ec4130b432ae0e46d7da387efa67829613
Abhishek-tara <abhishektara.ak47@gmail.com>
2023-02-23 14:26:04 +0530

    Added method to create Linked list

--- Program.cs
+++ Program.cs
@@ -7,10 +7,9 @@
             Console.WriteLine("Welcome to the Linked List Problems");
 
             LinkedList linked = new LinkedList();
-            linked.Add(10);
-            linked.Add(20);
+            linked.Add(56);
             linked.Add(30);
-            linked.Add(50);
+            linked.Add(70);
 
             linked.Display();
         }
