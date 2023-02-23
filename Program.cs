0b56697736620eb4128b6d0a8ec04363e54855e1
Abhishek-tara <abhishektara.ak47@gmail.com>
2023-02-23 13:54:47 +0530

    Added Program.cs

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace LinkedListDemo
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Linked List Problems");
+        }
+    }
+}
