315b5ce69b6f20e10843b44c6b88e888ed986a65
Abhishek-tara <abhishektara.ak47@gmail.com>
2023-02-24 11:49:45 +0530

    Deleting First Element form the Linked list

--- Program.cs
+++ Program.cs
@@ -10,12 +10,13 @@
 
             LinkedList linked = new LinkedList();
             linked.Add(56);
+            linked.Add(30);
             linked.Add(70);
             linked.Display();
 
-            Console.WriteLine("After Inserting:");
+            Console.WriteLine("After Poping First element..");
 
-            linked.AddAtPosition(2, 30);
+            linked.Pop();
             linked.Display();
         }
     }
